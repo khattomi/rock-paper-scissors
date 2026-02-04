2c924b1b069616325ddfb3ff506db49984d88509
khattomi <218912540+khattomi@users.noreply.github.com>
2026-02-04 08:22:21 +0200

    declare function for playing single round and modify computerCoice function

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //create a function that randomly returns "rock", "paper" or "scissors".
 function getComputerChoice() {
+    let computerChoice = "";
     let i = Math.floor(Math.random() * 3);
     if (i === 0) {
-        console.log("rock");
+        computerChoice = "rock";
     } else if (i === 1) {
-        console.log("paper");
+        computerChoice ="paper";
     } else {
-        console.log("scissors");
-    }
+        computerChoice = "scissors";
+    } 
+    return computerChoice;
 }
 
 //create a function that takes user choice and returns it.
@@ -20,3 +22,18 @@ function getHumanChoice () {
 let humanScore = 0;
 let computerScore = 0;
 
+//create a function that takes human and computer choices as arguments, plays a single round, increments the round winner's score and logs a winner announcement.
+function playRound(humanChoice, computerChoice) {
+    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+        humanScore++;
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+    } else if ((computerChoice === "rock" && humanChoice === "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
+        computerScore++;
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    }
+}
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
