53e941a721c4ec6288e3f18393502256fac5ea83
khattomi <218912540+khattomi@users.noreply.github.com>
2026-02-04 08:51:44 +0200

    Declare function playGame for playing the entire game

--- script.js
+++ script.js
@@ -18,22 +18,30 @@ function getHumanChoice () {
     return choice.toLowerCase();
 }
 
-//declare two variables to keep track of the players score.
-let humanScore = 0;
-let computerScore = 0;
+//declare a function that calls playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
+function playGame () {
+    //declare two variables to keep track of the players score.
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i=0; i < 5; i++) {
+        //create a function that takes human and computer choices as arguments, plays a single round, increments the round winner's score and logs a winner announcement.
+        function playRound(humanChoice, computerChoice) {
+            if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
+                humanScore++;
+                console.log("You win! " + humanChoice + " beats " + computerChoice);
+            } else if ((computerChoice === "rock" && humanChoice === "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
+                computerScore++;
+                console.log("You lose! " + computerChoice + " beats " + humanChoice);
+            }
+        }
 
-//create a function that takes human and computer choices as arguments, plays a single round, increments the round winner's score and logs a winner announcement.
-function playRound(humanChoice, computerChoice) {
-    if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "scissors" && computerChoice === "paper")) {
-        humanScore++;
-        console.log("You win! " + humanChoice + " beats " + computerChoice);
-    } else if ((computerChoice === "rock" && humanChoice === "scissors") || (computerChoice === "paper" && humanChoice === "rock") || (computerChoice === "scissors" && humanChoice === "paper")) {
-        computerScore++;
-        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        playRound(humanChoice, computerChoice);
     }
+    //print the winner
+    (humanScore > computerScore) ? console.log("Congratulations! you are the winner.") : console.log("Hard luck! I hope you do better next time.");
 }
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
